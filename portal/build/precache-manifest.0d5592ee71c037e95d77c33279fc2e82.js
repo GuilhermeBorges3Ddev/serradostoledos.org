ef55697661f3fe3050fecf5611c232dd2f792059
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-10-15 06:26:49 -0300

    public/ folder generated

---
+++ portal/build/precache-manifest.0d5592ee71c037e95d77c33279fc2e82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42167887f6c94da53cd9aaf7cf27f3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b155fd5d9c8e9f70261",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "91bf9ba916788d37876e",
+    "url": "/static/css/main.f027c75e.chunk.css"
+  },
+  {
+    "revision": "8b155fd5d9c8e9f70261",
+    "url": "/static/js/2.8c795d3d.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.8c795d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91bf9ba916788d37876e",
+    "url": "/static/js/main.e40d5937.chunk.js"
+  },
+  {
+    "revision": "c953884e1a18ff6b3160",
+    "url": "/static/js/runtime-main.0418dd04.js"
+  },
+  {
+    "revision": "c69ae8bdea68799b9000dcc38f05eaa9",
+    "url": "/static/media/home_bg.c69ae8bd.jpg"
+  }
+]);
