8eb20b7c7eb98ea53f5bb91f43db3fb3181bf1d5
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-10-15 06:21:14 -0300

    Under construction version finished

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="App-body" >
+          <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+            <CardTitle>
+              <h1 className="text-warning"><i>serradostoledos.org</i></h1>
+            </CardTitle>
+            <CardText className="under-construction-txt">
+              <p>Ao responder o questionário saberemos o que a plataforma deve ter...</p>
+              <p>Construiremos a plataforma com base nessas <u>respostas</u>!!!</p>
+            </CardText>
+            <Button>
+                <a
+                  className="App-link"
+                  href="https://forms.gle/rbKdZStyWd5vVeVb6"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Clique para responder nosso questionário
+                </a>
+            </Button>
+          </Card>
+        </div>
+
       </header>
-    </div>
+    </div>  
   );
 }
 
