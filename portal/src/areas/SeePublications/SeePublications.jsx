c3480a953df7cf7029d1d5c799df1461928b8de5
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-11-27 03:46:39 -0300

    Basic structure to receive the MongoDB request was done

--- portal/src/areas/SeePublications/SeePublications.jsx
+++ portal/src/areas/SeePublications/SeePublications.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
-import { Card, CardTitle } from 'reactstrap';
+
+import { 
+        Card, 
+        CardTitle,
+        ListGroup, 
+        ListGroupItem, 
+        ListGroupItemHeading, 
+        ListGroupItemText 
+} from 'reactstrap';
+
+//Using the <<iconify.design>> library of React
+import { Icon } from '@iconify/react';
+import listStatus from '@iconify-icons/mdi/list-status';
+
+/* 
+    These <<react-custom-scrollbars>> library was imported to threat the backend return of N posts... 
+    He had to wrap all content to work well.
+*/
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import HomeNavbar from '../../components/Navbar/HomeNavbar';
+
+import "./SeePublications.scss";
 
 export default function SeePublications() {
     return (
@@ -12,12 +32,57 @@ export default function SeePublications() {
             <header className="Wrapper-header">
                 <div className="Wrapper-body" >
                     <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-                    <CardTitle>
-                        <h1 className="text-warning text-capitalize">
-                            em construção
-                        </h1>
-                        <i>Área destinada para leitura de todas as publicações...</i>
-                    </CardTitle>
+                    
+                        <CardTitle>
+                            <h3 className="text-warning d-flex justify-content-around align-items-center">
+                                
+                                <div>
+                                    <Icon icon={listStatus} width={60} height={60} />
+                                </div>
+
+                                <div className="ml-5">
+                                    <i>{"  "}Veja as publicações que os usuários fizeram em tempo-real:</i>
+                                </div>
+                                
+                            </h3>
+                        </CardTitle>
+
+
+                        <Scrollbars 
+                            style={{ height: 350 }}
+                            renderThumbVertical={props => <div {...props} className="thumb-horizontal"/>}
+                        >
+                                <ListGroup>
+                                
+                                    <ListGroupItem className="bg-dark my-2">      
+                                        <ListGroupItemHeading className="d-flex justify-content-start">
+                                            List group item heading
+                                        </ListGroupItemHeading>
+                                        <ListGroupItemText>
+                                            Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
+                                        </ListGroupItemText>
+                                    </ListGroupItem>
+                                    
+                                    <ListGroupItem className="bg-dark my-2">
+                                        <ListGroupItemHeading className="d-flex justify-content-start">
+                                            List group item heading
+                                        </ListGroupItemHeading>
+                                        <ListGroupItemText>
+                                            Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
+                                        </ListGroupItemText>
+                                    </ListGroupItem>
+                                    
+                                    <ListGroupItem className="bg-dark my-2">
+                                        <ListGroupItemHeading>List group item heading</ListGroupItemHeading>
+                                        <ListGroupItemText>
+                                        Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
+                                        </ListGroupItemText>
+                                    </ListGroupItem>
+                            
+                                </ListGroup>
+
+                        </Scrollbars>  
+
                     </Card>
                 </div>
             </header>
